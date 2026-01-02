568b898bb62bed9469c9ff9b882c7b309435bb2a
timmyd2434 <timmyd2434@gmail.com>
2026-01-01 21:26:27 -0500

    Add CORS Proxy for Dev

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,13 @@ export default defineConfig({
   server: {
     host: true, // Listen on all local IPs
     allowedHosts: true, // Allow any host
+    proxy: {
+      '/vyos': {
+        target: 'https://192.168.0.22', // Proxy to your Router IP
+        changeOrigin: true,
+        secure: false, // Allow self-signed certs
+        rewrite: (path) => path.replace(/^\/vyos/, ''),
+      },
+    }
   }
 })
